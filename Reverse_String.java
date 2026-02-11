289a3b6285e0fea972faf799d020bcb381a2a0fd
anshul1144 <anshulspandey2004.com>
2026-02-11 12:12:42 +0530

    Reverse_String

---
+++ Reverse_String.java
@@ -0,0 +1,24 @@
+// Reverse a String (Without using inbuilt reverse method)
+
+import java.util.Scanner;
+
+class Reverse_String {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        // Loop from last character to first
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+
+        sc.close();
+    }
+}
